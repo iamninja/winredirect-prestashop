531d63df4e924ea8b40c7ceaf7c3d4b9e70361a4
Vagios Vlachos <bagios234@gmail.com>
2015-09-16 00:05:03 +0300

    Create module's main class

---
+++ winbankredirect.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+* Module's main class
+*/
+class WinbankRedirect extends PaymentModule
+{
+
+	function __construct()
+	{
+		// Module details
+		$this->name = 'winbankredirect';
+		$this->tab = 'payment_gateways';
+		$this->version = '0.1';
+		$this->author = 'Vagios Vlachos';
+		$this->bootstrap = true;
+
+		// Call parent
+		parent::__construct();
+
+		// More details
+		$this->displayName = $this->l('Winbank Redirect');
+		$this->description = $this->l('Winbank payment module, using redirect method');
+	}
+}
