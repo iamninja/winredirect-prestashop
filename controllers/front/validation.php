37a73cdb52089efd415fd5b290ac819c83175d6a
Vagios Vlachos <bagios234@gmail.com>
2015-10-03 17:09:46 +0300

    Set order states at validation controller

--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -29,6 +29,14 @@ class WinbankRedirectValidationModuleFrontController extends ModuleFrontControll
 		if(!($this->haskKeyCalculation($data) == $data['HashKey']))
 			$this->returnError('There was an error.');
 
+		// Get more data
+		$cart = $this->context->cart;
+		$currency = $this->context->currency;
+		$customer = new Costumer($cart->id_customer);
+		$total = (float)$cart->getOrderTotal(true, Cart::BOTH);
+		$extra_vars = array();
+		// Maybe add some check for the data above
+
 		// TODO
 		// Check cart and customer validity (using $data['Parameters'])
 		// If check fails on this stage the order has to be placed as problematic
@@ -36,10 +44,15 @@ class WinbankRedirectValidationModuleFrontController extends ModuleFrontControll
 		// TODO
 		// If transaction type (WINBANKREDIRECT_OPTS_TRANSACTIONTYPE) is preauthorization
 		// an special order state must be used
+		if (Configuration::get('WINBANKREDIRECT_OPTS_TRANSACTIONTYPE') == 0)
+			$this->module->validateOrder($cart->id, Configuration::get('PS_OS_WINBANKREDIRECT_PREAUTHORIZATION'), $total, $this->module->displayName, NULL, $extra_vars, (int)$currency->id, false, $customer->secure_key);
 
 		// TODO
 		// If transaction type (WINBANKREDIRECT_OPTS_TRANSACTIONTYPE) is sale
-		// a normal (i.e. one of the prestashop's built in) state can be used
+		// a normal (i.e. one of the prestashop's built in) state can be used.
+		// Gonna use PS_OS_PREPARATION
+		if (Configuration::get('WINBANKREDIRECT_OPTS_TRANSACTIONTYPE') == 1)
+			$this->module->validateOrder($cart->id, Configuration::get('PS_OS_PREPARATION'), $total, $this->module->displayName, NULL, $extra_vars, (int)$currency->id, false, $customer->secure_key);
 	}
 
 	public function returnError($result)
