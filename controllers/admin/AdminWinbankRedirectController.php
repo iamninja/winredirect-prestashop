1bfd6e1d7d3efdb19ac710b1ec5e25e5d288993e
Vagios Vlachos <bagios234@gmail.com>
2015-10-08 15:19:07 +0300

    Show only successful winbankredirect_transaction entries on admin tab

--- controllers/admin/AdminWinbankRedirectController.php
+++ controllers/admin/AdminWinbankRedirectController.php
@@ -11,6 +11,19 @@ class AdminWinbankRedirectController extends ModuleAdminController
 		$this->table = 'winbankredirect_transaction'; // database name
 		$this->className = 'WinbankRedirectTransaction'; // ObjectModel class
 		$this->addRowAction('view'); // row actions
+
+		// Enable bootstrap
+		$this->bootstrap = true;
+
+		// Call parent method
+		parent::__construct();
+
+		// Get order states names
+		$statuses = OrderState::getOrderStates((int)$this->context->language->id);
+        foreach ($statuses as $status) {
+            $statuses_array[$status['id_order_state']] = $status['name'];
+        }
+
 		$this->fields_list = array(
 			'id_winbankredirect_transaction' => array(
 				'title' => $this->l('ID'),
@@ -18,41 +31,42 @@ class AdminWinbankRedirectController extends ModuleAdminController
 				'width' => 25,
 				'search' => false
 			),
-			'id_cart' => array(
-				'title' => $this->l('Cart ID'),
-				'width' => 25,
-				'search' => false
-			),
 			'id_order' => array(
 				'title' => $this->l('Order ID'),
-				'width' => 25
+				'width' => 30
 			),
 			'current_state' => array(
-				'title' => $this->l('Status'),
-				'width' => 100,
-				'search' => true,
-				'type' => 'select'
-			),
+                'title' => $this->l('Status'),
+                'type' => 'select',
+                'list' => $statuses_array,
+                'filter_key' => 'os!id_order_state',
+                'filter_type' => 'int',
+                'order_key' => 'current_state',
+                'callback' => 'getOrderStateName',
+                'color' => 'red'
+            ),
 			'merchant_reference' => array(
 				'title' => $this->l('Merchant Reference'),
-				'width' => 100
+				'width' => 80
 			),
 			'installments' => array(
 				'title' => $this->l('Installments'),
-				'width' => 25
+				'width' => 30,
+				'align' => 'center'
 			),
 			'is_preauthorization' => array(
 				'title' => $this->l('Preauthorization'),
-				'width' => 25,
+				'width' => 10,
+				'align' => 'center',
 				'type' => 'bool'
 			)
 		);
 
-		// Enable bootstrap
-		$this->bootstrap = true;
+		$this->_where = 'AND a.`successful` = 1';
 
-		// Call parent method
-		parent::__construct();
+		$this->_join = '
+			LEFT JOIN `'._DB_PREFIX_.'order_state` os ON (os.`id_order_state` = a.`current_state`)
+			LEFT JOIN `'._DB_PREFIX_.'order_state_lang` osl ON (os.`id_order_state` = osl.`id_order_state` AND osl.`id_lang` = '.(int)$this->context->language->id.')';
 	}
 
 	public function renderView()
@@ -72,5 +86,32 @@ class AdminWinbankRedirectController extends ModuleAdminController
     	$link .= '&vieworder&id_order='.$id_order;
     	$this->redirect_after = $link;
     	$this->redirect();
+    }
+
+    public function renderList()
+    {
+    	// Get order states names
+		$statuses = OrderState::getOrderStates((int)$this->context->language->id);
+        foreach ($statuses as $status) {
+            $statuses_array[$status['id_order_state']] = $status['name'];
+        }
+
+    	$this->tpl_list_vars['updateOrderStatus_mode'] = true;
+        $this->tpl_list_vars['current_states'] = $statuses_array;
+        $this->tpl_list_vars['REQUEST_URI'] = $_SERVER['REQUEST_URI'];
+        $this->tpl_list_vars['POST'] = $_POST;
+
+        return parent::renderList();
+    }
+
+    public function getOrderStateName($echo, $tr)
+    {
+    	// Get order states names
+		$statuses = OrderState::getOrderStates((int)$this->context->language->id);
+        foreach ($statuses as $status) {
+            $statuses_array[$status['id_order_state']] = $status['name'];
+        }
+
+    	return $statuses_array[$echo];
     }
 }
